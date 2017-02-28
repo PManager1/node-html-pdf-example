9feee4a2c59a6660b5b0ad94373f3feb4158e14c
jpca999 <jpca999@gmail.com>
2017-02-28 00:41:16 -0500

    -

--- test/gencontract.js
+++ test/gencontract.js
@@ -8,13 +8,13 @@ var html = fs.readFileSync('contract.html', 'utf8');
 
 // var options = { format: 'Letter' , "timeout": 30000 };
 
-var options = { "height": "10.63in", "width": "10.16in", "timeout": 30000, };
+var options = {  "width": "16.16in", "height": "16.63in", "timeout": 100000,  };
 
 
 
 
 // pdf.create(html, options).toFile('LETTER.pdf', function(err, res) {	
-pdf.create(html, options).toFile('contract_10.16by_10.63.pdf', function(err, res) {	
+pdf.create(html, options).toFile('contract_16.16by_16.63.pdf', function(err, res) {	
   if (err) return console.log(err);
   console.log(res); // { filename: '/app/businesscard.pdf' }
 });
