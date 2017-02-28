579e344f5531daea5319627dcc675e693a408d5f
jpca999 <jpca999@gmail.com>
2017-02-28 00:25:07 -0500

    -

--- test/gencontract.js
+++ test/gencontract.js
@@ -1,11 +1,20 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
-// var html = fs.readFileSync('businesscard.html', 'utf8');
-var html = fs.readFileSync('contract.html', 'utf8');
-var options = { format: 'Letter' };
 
-// pdf.create(html, options).toFile('businesscard.pdf', function(err, res) {
-pdf.create(html, options).toFile('contract.pdf', function(err, res) {	
+// var pdf = require('phantom');
+
+var html = fs.readFileSync('contract.html', 'utf8');
+
+
+var options = { format: 'Letter' , "timeout": 30000 };
+
+// var options = { "height": "10.63in", "width": "8.16in", "timeout": 30000, };
+
+
+
+
+pdf.create(html, options).toFile('LETTER.pdf', function(err, res) {	
+// pdf.create(html, options).toFile('contract_8.16by_10.63.pdf', function(err, res) {	
   if (err) return console.log(err);
   console.log(res); // { filename: '/app/businesscard.pdf' }
 });
