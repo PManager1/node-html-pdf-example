07352303c59e04c01eb8154e43cc440712fc705e
jpca999 <jpca999@gmail.com>
2017-02-28 00:27:30 -0500

    increasing the width helped

--- test/gencontract.js
+++ test/gencontract.js
@@ -6,15 +6,15 @@ var pdf = require('html-pdf');
 var html = fs.readFileSync('contract.html', 'utf8');
 
 
-var options = { format: 'Letter' , "timeout": 30000 };
+// var options = { format: 'Letter' , "timeout": 30000 };
 
-// var options = { "height": "10.63in", "width": "8.16in", "timeout": 30000, };
-
-
+var options = { "height": "10.63in", "width": "10.16in", "timeout": 30000, };
 
 
-pdf.create(html, options).toFile('LETTER.pdf', function(err, res) {	
-// pdf.create(html, options).toFile('contract_8.16by_10.63.pdf', function(err, res) {	
+
+
+// pdf.create(html, options).toFile('LETTER.pdf', function(err, res) {	
+pdf.create(html, options).toFile('contract_10.16by_10.63.pdf', function(err, res) {	
   if (err) return console.log(err);
   console.log(res); // { filename: '/app/businesscard.pdf' }
 });
